688ee2a07a903e52733e58713488d91dc8bb6cc6
Gigglande-Fresh <gigglande-fresh@gigglandefresh-VirtualBox.(none)>
2013-04-10 23:46:03 -0400

    Accidental commit of wrong version.

--- lolcat.rb
+++ lolcat.rb
@@ -3,37 +3,43 @@ require 'open-uri'
 
 class LolCatViewer
 
-  def self.run(links=nil)
-    loldocs = Nokogiri::HTML(open("http://www.lolcats.com/gallery/new.html"))
-    iterable_cats = doc.css('.gallery-item img')
+	def self.run(links=nil)
+		loldocs = Nokogiri::HTML(open("http://www.lolcats.com/gallery/new.html"))
+		iterable_cats = loldocs.css('.gallery-item img') 
 
-    if (not links) or (links < iterable_cats.length)
-      links = 1
-    end
+		if (not links) or (links > iterable_cats.length)
+			links = 1
+		end
 
-    $i = 0
-    cat_array = []
+		$i = 0
+		cat_array = []
+		
+		while $i < (iterable_cats.length) do 
+			cat_array.push(iterable_cats[$i])
+			$i += 1
+		end
 
-    while $i < iterable_cats.length do
-      cat_link = Nokogiri::HTML(open("http://www.lolcats.com#{iterable_cats[$i]['href']}")).css(".picture-container img")[0]
-      cat_array.push(cat_link)
-      $i += 1
-    end
+		cat_array.shuffle!
 
-    cat_indexor = (0..cat_array.length).to_a.shuffle
-    
-    $i = 0
-    while $i < cat_indexor do
-      cat = cat_indexor[$i].pop
-      puts "Title:#{cat['alt']} Link: http://www.lolcats.com#{cat['src']}"
-      $i += 1
-
-    end
-  end
+        $i = cat_array.length - links
+        $fixed_length = cat_array.length
+		while $i < $fixed_length do 
+			cat = cat_array.pop
+			puts "Title:#{cat['alt']} Link: http://www.lolcats.com#{cat['src']}"
+            $i += 1
+		end
+	end
 end
 
-
-
-
-
-
+puts '---------------------------------'
+puts 'Run with no argument.'
+puts '---------------------------------'
+LolCatViewer.run()
+puts '---------------------------------'
+puts 'Run with 5.'
+puts '---------------------------------'
+LolCatViewer.run(5)
+puts '---------------------------------'
+puts 'Run with an amount larger than images available.'
+puts '---------------------------------'
+LolCatViewer.run(5000)
